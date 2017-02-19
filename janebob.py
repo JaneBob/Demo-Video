3cd167e94b402dbfdeb2520a0b17ffafe0d0292c
JaneBob <cookdr@stny.rr.com>
2017-02-19 16:32:17 -0500

    Create janebob.py

---
+++ janebob.py
@@ -0,0 +1,1 @@
+import jimbobimport unittestdef three_times_x_plus_two(x):    y = jimbob.threetimes(x)    if y is None:        return None    else:        return y + 2class MyTest(unittest.TestCase):    def test(self):        self.assertEqual(three_times_x_plus_two(3), 11)        self.assertEqual(three_times_x_plus_two(3.1), 11.3)        self.assertEqual(three_times_x_plus_two("a"), None)
