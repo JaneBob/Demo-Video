ff455f0ce924a59d862b25b7c192bc914dd782c3
JaneBob <cookdr@stny.rr.com>
2017-02-19 16:35:09 -0500

    Adding janebob.py
    Waiting for jimbob's code

--- janebob.py
+++ janebob.py
@@ -1,1 +1,15 @@
-import jimbobimport unittestdef three_times_x_plus_two(x):    y = jimbob.threetimes(x)    if y is None:        return None    else:        return y + 2class MyTest(unittest.TestCase):    def test(self):        self.assertEqual(three_times_x_plus_two(3), 11)        self.assertEqual(three_times_x_plus_two(3.1), 11.3)        self.assertEqual(three_times_x_plus_two("a"), None)
+import jimbob
+import unittestdef 
+three_times_x_plus_two(x):    
+  y = jimbob.threetimes(x)    
+  if y is None:        
+    return None    
+  else:        
+    return y + 2
+
+  
+  class MyTest(unittest.TestCase):    
+    def test(self):        
+      self.assertEqual(three_times_x_plus_two(3), 11)        
+      self.assertEqual(three_times_x_plus_two(3.1), 11.3)        
+      self.assertEqual(three_times_x_plus_two("a"), None)
